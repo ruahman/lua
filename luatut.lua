6ef99bb3c009f5aa050654c32884f022f0848517
Diego <diego@velascommerce.com>
2023-01-26 15:07:24 -0400

    lua tut

---
+++ luatut.lua
@@ -0,0 +1,12 @@
+--comments
+--[[
+multi line comments
+multi line comments
+multi line comments
+multi line comments
+multi line comments
+]]
+print("hello world")
+name = "Diego"
+io.write("my name is ", name, #name)
+print("my name is ", #name)
